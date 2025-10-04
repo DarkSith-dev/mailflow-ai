45862bcb4e2d02dec49315baad49750d1bf16c3b
Diego Alves Rodrigues <51385145+DarkSith-dev@users.noreply.github.com>
2025-10-04 14:55:35 -0300

    fix bugs

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ from gerenciar_arquivo import gerar_arquivo_gemini
 from selecionar_persona import personas
 from flask import Flask, jsonify,render_template, request
 
-app = Flask(__name__)
 
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
 
 
 from time import sleep
